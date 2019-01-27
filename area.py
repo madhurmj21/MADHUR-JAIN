b29aeb8ee1e95fc7cfae18c6a0ea3ec3db5de8dd
madhurmj21 <46773750+madhurmj21@users.noreply.github.com>
2019-01-27 21:38:50 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+print("program to find area and perimeter of circle with the help of given radius")
+
+radius = input("Enter radius of a circle:")
+a = 3.14*(radius**2)
+print("area of circle:{}".format(a))
+print("perimeter of circle:{}".format(2*3.14*radius))
