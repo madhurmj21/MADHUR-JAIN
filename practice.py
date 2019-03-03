eb9ffdc8151bc1a73205d97e3cfd88dc1994fb8c
madhurmj21 <46773750+madhurmj21@users.noreply.github.com>
2019-03-03 12:30:26 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,35 @@
+#print('Twinkle, twinkle, little star,\n        How I wonder what you are! \n          Up above the world so high,\n          Like a diamond in the sky.\n Twinkle, twinkle, little star,\n        How I wonder what you are')
+
+
+#import datetime
+#now = datetime.date(2019,2,25)
+#print(now)
+"""
+radius = eval(input('enter radius of a circle'))
+a = (2*3.14*radius)
+b = 3.14*(radius**2)
+print ('area of circle {}, circumference of circle {}'.format (a, b))
+"""
+
+"""
+first_name = input('enter your first name:')
+last_name = input('enter your last name:')
+print ('{}  {}'.format (last_name,first_name))
+"""
+"""
+nums = eval(input('enter a series of number'))
+print(nums)
+k_l = tuple(nums)
+print(k_l)
+number_list = list(nums)
+#print(number_list)
+print ('list = {}'.format(number_list))
+"""
+
+"""
+
+fileName = input(' enter the file name with extension:')
+file,ext=fileName.split('.')
+print('extentsion of file name {}'.format(ext))
+
+"""
